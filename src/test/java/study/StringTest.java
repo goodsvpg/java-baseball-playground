818b91d75ee72ee3b4653b62fae056322dbbaa5a
goodsvpg <sabai.dev.me@kakao.com>
2021-09-01 10:25:46 +0900

    string test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,50 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void splitStringContainsCorrectValue() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void splitStringContainsExactlyCorrectValue() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        char actual = "abc".charAt(1);
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("test chatAt() when IndexOutOfBoundsException throw")
+    void testIndexOutOfBoundsExceptionUsingCharAt() {
+        assertThatThrownBy(() -> { 
+                            char wrong = "abc".charAt(4); 
+                            assertThat(wrong).isEqualTo('c');
+                        })
+                        .isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
