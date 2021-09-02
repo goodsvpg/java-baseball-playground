4b333fc7f23890ce04c9e99e0d6ed0fd162f3172
goodsvpg <sabai.dev.me@kakao.com>
2021-09-03 07:12:06 +0900

    test set Test contain using ParamterizedTest

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +20,6 @@ public class SetTest {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
-        numbers.add(4);
     }
 
     @Test
@@ -30,9 +30,13 @@ public class SetTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void contains(int testNumber) {
+    void containsProvidedValueSource(int testNumber) {
         assertThat(numbers.contains(testNumber)).isTrue();
     }
 
-    
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void containsProvidedCsvSource(int testNumber, boolean expected) {
+        assertThat(numbers.contains(testNumber)).isEqualTo(expected);
+    }
 }
